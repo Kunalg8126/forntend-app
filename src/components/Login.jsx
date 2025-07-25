37fd056c017c1b55dc516f540e78d548a1efac8d
Prince Gupta <kunalgupta1354@gmail.com>
2025-07-25 21:10:27 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         
        
         try {
-            const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/login`, {
+            const res = await fetchfetch(`https://classifyapp.onrender.com/api/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
